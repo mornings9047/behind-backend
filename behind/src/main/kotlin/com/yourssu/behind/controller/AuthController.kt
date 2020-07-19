4aa966fe0ffe7dc18f4d51ffd301e989205400b4
JoonHee <mornings9047@naver.com>
2020-07-20 00:22:25 +0900

    회원가입 api 구현 중-ResponseStatusException을 사용하여 예외처리

--- behind/src/main/kotlin/com/yourssu/behind/controller/AuthController.kt
+++ behind/src/main/kotlin/com/yourssu/behind/controller/AuthController.kt
@@ -1,14 +1,10 @@
 package com.yourssu.behind.controller
 
-import com.yourssu.behind.exception.InvalidPasswordException
-import com.yourssu.behind.exception.InvalidSchoolIdException
-import com.yourssu.behind.exception.InvalidUsernameException
-import com.yourssu.behind.exception.UserAlreadyExistsException
 import com.yourssu.behind.model.dto.UserSignUpRequestDto
 import com.yourssu.behind.service.AuthService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
@@ -19,14 +15,8 @@ class AuthController {
     fun signUpNewUser(@RequestBody signUpRequestDto: UserSignUpRequestDto) {
         try {
             return authService.signUp(signUpRequestDto)
-        } catch (e: InvalidSchoolIdException) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "유효하지 않은 학번입니다", e)
-        } catch (e: InvalidUsernameException) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "유효하지 않은 아이디입니다", e)
-        } catch (e: InvalidPasswordException) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "유효하지 않은 비밀번호입니다", e)
-        } catch (e: UserAlreadyExistsException) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 존재하는 학번입니다", e)
+        } catch (e: ResponseStatusException) {
+            throw ResponseStatusException(e.status, e.message, e)
         }
     }
 
