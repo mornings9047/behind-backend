92b34fa3cd611cdd58f3ae6b51df740aa1e82a6e
김태준 <jamma.urssu@gmail.com>
2020-07-23 20:50:19 +0900

    AuthValidFunction

--- behind/src/main/kotlin/com/yourssu/behind/service/auth/AuthService.kt
+++ behind/src/main/kotlin/com/yourssu/behind/service/auth/AuthService.kt
@@ -2,44 +2,25 @@ package com.yourssu.behind.service
 
 import com.yourssu.behind.exception.user.PasswordNotMatchedException
 import com.yourssu.behind.exception.user.UserNotExistsException
-import com.yourssu.behind.exception.user.*
 import com.yourssu.behind.model.dto.UserSignInRequestDto
 import com.yourssu.behind.model.dto.UserSignUpRequestDto
 import com.yourssu.behind.model.entity.user.User
-import com.yourssu.behind.repository.UserRepository
+import com.yourssu.behind.repository.user.UserRepository
+import com.yourssu.behind.service.auth.function.AuthValidFunction
 import org.mindrot.jbcrypt.BCrypt
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class AuthService @Autowired constructor(val userRepository: UserRepository) {
-
-    fun isUserSignUpRequestDtoValid(userSignUpRequestDto: UserSignUpRequestDto): Boolean {
-        if (!isSchoolIdValid(userSignUpRequestDto.schoolId))
-            throw InvalidSchoolIdException()
-        if (userRepository.existsBySchoolId(userSignUpRequestDto.schoolId))
-            throw UserAlreadyExistsException()
-        if (!isPasswordValid(userSignUpRequestDto.password))
-            throw InvalidPasswordException()
-        return true
-    }
-
-    fun isSchoolIdValid(schoolId: String): Boolean {
-        val regex = Regex("^(20)([0-9]){6}$")
-        return regex.matches(schoolId)
-    }
-
-    fun isPasswordValid(password: String): Boolean {
-        val regex = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,20}$")
-        return regex.matches(password)
-    }
+    val authValidFunction: AuthValidFunction = AuthValidFunction(userRepository)
 
     fun signUp(userSignUpRequestDto: UserSignUpRequestDto) {
-        if (isUserSignUpRequestDtoValid(userSignUpRequestDto))
+        if (authValidFunction.isUserSignUpRequestDtoValid(userSignUpRequestDto))
             userRepository.save(User(
-                    schoolId = userSignUpRequestDto.schoolId,
-                    password = BCrypt.hashpw(userSignUpRequestDto.password, BCrypt.gensalt())
-            ))
+                        schoolId = userSignUpRequestDto.schoolId,
+                        password = BCrypt.hashpw(userSignUpRequestDto.password, BCrypt.gensalt())
+                ))
     }
 
     fun signIn(userSignInRequestDto: UserSignInRequestDto): Boolean {
---
+++ behind/src/main/kotlin/com/yourssu/behind/service/auth/function/AuthValidFunction.kt
@@ -0,0 +1,29 @@
+package com.yourssu.behind.service.auth.function
+
+import com.yourssu.behind.exception.user.InvalidPasswordException
+import com.yourssu.behind.exception.user.InvalidSchoolIdException
+import com.yourssu.behind.exception.user.UserAlreadyExistsException
+import com.yourssu.behind.model.dto.UserSignUpRequestDto
+import com.yourssu.behind.repository.user.UserRepository
+
+class AuthValidFunction(val userRepository: UserRepository) {
+    fun isUserSignUpRequestDtoValid(userSignUpRequestDto: UserSignUpRequestDto): Boolean {
+        if (!isSchoolIdValid(userSignUpRequestDto.schoolId))
+            throw InvalidSchoolIdException()
+        if (userRepository.existsBySchoolId(userSignUpRequestDto.schoolId))
+            throw UserAlreadyExistsException()
+        if (!isPasswordValid(userSignUpRequestDto.password))
+            throw InvalidPasswordException()
+        return true
+    }
+
+    fun isSchoolIdValid(schoolId: String): Boolean {
+        val regex = Regex("^(20)([0-9]){6}$")
+        return regex.matches(schoolId)
+    }
+
+    fun isPasswordValid(password: String): Boolean {
+        val regex = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,20}$")
+        return regex.matches(password)
+    }
+}
