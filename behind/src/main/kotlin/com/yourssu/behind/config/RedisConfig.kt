2948844664e98da7dfec3bebf256880eb4b03409
김태준 <jamma.urssu@gmail.com>
2020-08-16 14:26:49 +0900

    redis,s3

--- behind/src/main/kotlin/com/yourssu/behind/config/RedisConfig.kt
+++ behind/src/main/kotlin/com/yourssu/behind/config/RedisConfig.kt
@@ -1,20 +1,32 @@
 package com.yourssu.behind.config
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
-class RedisConfig {
+class RedisConfig @Autowired constructor(@Value("\${spring.redis.host}")
+                                         val redisHost: String,
+                                         @Value("\${spring.redis.port}")
+                                         val redisPort: Int,
+                                         @Value("\${spring.redis.password}")
+                                         val password: RedisPassword) {
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory()
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration()
+        redisStandaloneConfiguration.hostName = redisHost
+        redisStandaloneConfiguration.port = redisPort
+        redisStandaloneConfiguration.password = password
+        return LettuceConnectionFactory(redisStandaloneConfiguration)
     }
-
     @Bean
     fun redisTemplate(): RedisTemplate<String, Any> {
         val redisTemplate = RedisTemplate<String, Any>()
