0b8b05244da982849817498357b56aee4752c027
JoonHee <mornings9047@naver.com>
2020-09-07 23:38:28 +0900

    CommentController에 @PathVariable로 postId 추가

--- behind/src/main/kotlin/com/yourssu/behind/service/comment/function/ReportCommentFunction.kt
+++ behind/src/main/kotlin/com/yourssu/behind/service/comment/function/ReportCommentFunction.kt
@@ -1,21 +1,26 @@
 package com.yourssu.behind.service.comment.function
 
 import com.yourssu.behind.exception.comment.CommentNotExistsException
+import com.yourssu.behind.exception.post.PostNotContainsCommentException
+import com.yourssu.behind.exception.post.PostNotExistsException
 import com.yourssu.behind.exception.report.CommentAlreadyReportedException
 import com.yourssu.behind.model.entity.report.Report
-import com.yourssu.behind.model.entity.user.User
 import com.yourssu.behind.repository.comment.CommentRepository
 import com.yourssu.behind.repository.post.PostRepository
 import com.yourssu.behind.repository.report.ReportRepository
+import com.yourssu.behind.service.auth.JwtService
 
 class ReportCommentFunction(private val postRepository: PostRepository,
                             private val commentRepository: CommentRepository,
-                            private val reportRepository: ReportRepository) {
+                            private val reportRepository: ReportRepository,
+                            private val jwtService: JwtService) {
 
-    fun reportComment(user: User, commentId: Long) {
+    fun reportComment(postId: Long, commentId: Long) {
+        val user = jwtService.getUser()
+        val post = postRepository.findById(postId).orElseThrow { PostNotExistsException() }
         val comment = commentRepository.findById(commentId).orElseThrow { CommentNotExistsException() }
-        val post = postRepository.findByComments(comment).orElseThrow { CommentNotExistsException() }
-
+        if (!post.comments.contains(comment))
+            throw PostNotContainsCommentException()
         if (reportRepository.existsByCommentAndUser(comment, user))
             throw CommentAlreadyReportedException()
         if (comment.reportNum >= 2) {
